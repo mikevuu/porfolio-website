b80a914d91f99a29f702d78de3f5178618605df7
nguyenvuu1992 <nguyenvuu1992@gmail.com>
2022-07-21 21:20:46 +0700

    to 32:04

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,13 +2,17 @@ import './topbar.scss'
 
 export default function Topbar() {
     return (
-        <div className='topbar'>
+        <div className='topbar' id = 'topbar'>
             <div className='wrapper'>
                 <div className="left">
-                    <a href="#intro">abcssdf</a>
+                    <a href="#intro" className = 'logo'>Genius.</a>
+                    <div className="itemContainer">
+                        
+                    </div>
                 </div>
+                <h1> Javascript</h1>
                 <div className="right">
-
+                    this is right
                 </div>
             </div>
         </div>
